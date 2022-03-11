3185ea0b39f4c11743f9c7599b830e1b0f97ea33
Arif Rubayet <aruba055@uottawa.ca>
2022-03-10 20:42:02 -0500

    Fixing Footer and Updating Env

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
         <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
             <div>
 
-                <p>Copyright &copy; {FooterYear} All rights reserved</p>
+                <p> Copyright &copy; {FooterYear} All rights reserved</p>
             </div>
         </footer>
     )
